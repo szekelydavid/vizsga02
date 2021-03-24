0ad724b5ab3cd2dddb68d6bc6022d83f4aba7e16
zekely David Attila <szekelydavidattila@gmail.com>
2021-03-24 15:10:39 +0100

    *

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -5,6 +5,14 @@ import java.util.List;
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+        int sumOfOddNums=0;
+        for(int number : numbers)
+        {
+            int absnum=Math.abs(number);
+            if(absnum%2==1){
+                sumOfOddNums += number;
+            }
+        }
+        return sumOfOddNums;
     }
 }
